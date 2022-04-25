db1106e1beb2d7b9544c9abed642117a90baf5d1
“Mett-Barrgit config --global user.name “Mett-Barr <ohmylove347@gmail.comgit config --global user.name Mett-Barr>
2022-04-25 17:27:28 +0800

    4/25-3 Room

---
+++ app/src/main/java/com/example/english/data/newslist/Repository.kt
@@ -0,0 +1,15 @@
+package com.example.english.data.newslist
+
+import com.example.english.data.newslist.room.News
+import kotlinx.coroutines.flow.Flow
+
+interface Repository {
+
+    fun newsList(): Flow<List<News>>
+
+    fun getSize(): Flow<Int>
+
+    fun isEmpty(): Boolean
+
+    suspend fun addNews(news: News)
+}
