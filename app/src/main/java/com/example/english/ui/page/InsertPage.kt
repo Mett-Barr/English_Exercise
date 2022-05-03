3cc29967ee3f6dd859487273c37ed276f83ec822
“Mett-Barrgit config --global user.name “Mett-Barr <ohmylove347@gmail.comgit config --global user.name Mett-Barr>
2022-05-03 17:45:28 +0800

    5/3-3

--- app/src/main/java/com/example/english/ui/page/InsertPage.kt
+++ app/src/main/java/com/example/english/ui/page/InsertPage.kt
@@ -1,13 +1,18 @@
 package com.example.english.ui.page
 
-import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
@@ -15,7 +20,6 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
 import com.example.english.MainViewModel
-import com.example.english.article.defaultText.text
 import com.example.english.stringconverter.StringConverter
 import com.example.english.ui.components.FlatTextField
 import com.example.english.ui.components.OperationButton
@@ -30,9 +34,17 @@ fun InsertPage(viewModel: MainViewModel, navController: NavController) {
         mutableStateOf(false)
     }
 
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .systemBarsPadding()) {
+    fun addNews() {
+        viewModel.addNews(context)
+        StringConverter().stringToListNull(viewModel.draftContent.text)
+        navController.popBackStack()
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .systemBarsPadding()
+    ) {
 
         Text(
             "News",
@@ -68,52 +80,59 @@ fun InsertPage(viewModel: MainViewModel, navController: NavController) {
             },
             clickOK = {
                 openDialog = true
-//                viewModel.addNews(context)
-//                StringConverter().stringToListNull(viewModel.draftContent.text)
-//                navController.popBackStack()
             }
         )
     }
 
     if (openDialog) {
 
-//        val radioOptions = listOf("Tech", "Apple", "Google", "Meta","MircoSoft", "Amazon", "Netflix", "Tesla", "Elon Musk")
-        val radioOptions = listOf("A", "S", "D")
-        val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[0]) }
-
-        var test by remember {
-            mutableStateOf(Test.A)
+        var radioOption by remember {
+            mutableStateOf(Tag.TECH)
         }
 
         Dialog(onDismissRequest = { openDialog = false }) {
-            Card {
-                Column(modifier = Modifier.selectableGroup()) {
-//                radioOptions.forEach()
-                    for (tag in enumValues<Test>()) {
+            Card(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(MaterialTheme.colors.primaryVariant)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .selectableGroup()
+                        .padding(horizontal = 4.dp)
+                        .padding(top = 16.dp, bottom = 4.dp)
+                ) {
+                    Text(
+                        text = "Tag", style = Typography.h4, modifier = Modifier
+                            .align(Alignment.CenterHorizontally)
+                            .padding(8.dp)
+                    )
+                    for (it in enumValues<Tag>()) {
                         Row(
                             Modifier
                                 .fillMaxWidth()
                                 .height(56.dp)
+                                .padding(vertical = 4.dp)
+                                .clip(RoundedCornerShape(12.dp))
                                 .selectable(
-                                    selected = (tag == test),
+                                    selected = (it == radioOption),
                                     onClick = {
-                                        Log.d("!!!", tag.name)
-                                        test = tag
-                                        Log.d("!!!", tag.name + "\n\n ")
+                                        radioOption = it
+                                        addNews()
                                     },
                                     role = Role.RadioButton
                                 )
-                                .padding(horizontal = 16.dp),
+                                .padding(4.dp),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             RadioButton(
-                                selected = (tag.name == selectedOption),
+                                selected = (it == radioOption),
                                 onClick = null // null recommended for accessibility with screenreaders
                             )
                             Text(
-                                text = tag.name,
+                                text = it.tag,
                                 style = MaterialTheme.typography.body1.merge(),
-                                modifier = Modifier.padding(start = 16.dp)
+                                modifier = Modifier.padding(start = 16.dp),
                             )
                         }
                     }
@@ -123,7 +142,9 @@ fun InsertPage(viewModel: MainViewModel, navController: NavController) {
     }
 }
 
-enum class Test {
-    A, S, D
+enum class Tag(val tag: String) {
+    TECH("Tech"), APPLE("Apple"), GOOGLE("Google"), META("Meta"),
+    AMAZON("Amazon"), NETFLIX("Netflix"), TESLA("Tesla"),
+    ELON_MUSK("Elon Musk")
 }
 
