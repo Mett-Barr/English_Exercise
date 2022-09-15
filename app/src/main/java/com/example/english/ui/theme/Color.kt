c00e0153d4688b1c5232b8782be3002e90e920f0
Mett-Barr <ohmylove347@gmail.com>
2022-09-15 08:09:18 +0800

    9/15

--- app/src/main/java/com/example/english/ui/theme/Color.kt
+++ app/src/main/java/com/example/english/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.example.english.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val LightPrimary = Color(0xFFC1EDFF)
+val DarkPrimary = Color(0xFF004787)
+val LightSecondary = Color(0xFF004787)
 
 val Primary = Color(0xFFa9c7ff)
 val PrimaryVariant = Color(0xFF004787)
